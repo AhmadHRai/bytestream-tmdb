e9aff13908cb058bd39a723f87d01a889a40d7fb
Ahmad Alrai <ahmadhalraie@gmail.com>
2024-03-03 18:19:27 +0300

    component structure and routing

--- src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div className="App">Initial Cleanup</div>;
-}
-
-export default App;
---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
