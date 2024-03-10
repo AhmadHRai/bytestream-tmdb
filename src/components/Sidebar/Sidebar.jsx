444b4d9b2a0a5c4ba034e45cec122bed54ec1069
Ahmad Alrai <ahmadhalraie@gmail.com>
2024-03-10 19:13:36 +0300

    Add genre icons - sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,20 +12,18 @@ import {
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 
+import { useGetGenresQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
+
 const categories = [
   { label: 'Popular', value: 'popular' },
   { label: 'Top Rated', value: 'top_rated' },
   { label: 'Upcoming', value: 'upcoming' },
 ];
-const demoCategories = [
-  { label: 'Comedy', value: 'comedy' },
-  { label: 'Action', value: 'action' },
-  { label: 'Horror', value: 'horror' },
-  { label: 'Animation', value: 'animation' },
-];
 
 function Sidebar({ setMobileOpen }) {
   const theme = useTheme();
+  const { data, isFetching } = useGetGenresQuery();
 
   return (
     <>
@@ -42,13 +40,13 @@ function Sidebar({ setMobileOpen }) {
         {categories.map(({ label, value }) => (
           <Link key={value} style={{ color: theme.palette.text.primary, textDecoration: 'none' }} to="/">
             <ListItemButton onClick={() => {}}>
-              {/* <ListItemIcon>
+              <ListItemIcon>
                 <img
-                  src="/lightLogo.png"
-                  style={{ filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)' }}
+                  src={genreIcons[label.toLowerCase()]}
+                  style={{ filter: theme.palette.mode === 'light' ? 'dark' : 'invert(1)' }}
                   height={30}
                 />
-              </ListItemIcon> */}
+              </ListItemIcon>
               <ListItemText primary={label} />
             </ListItemButton>
           </Link>
@@ -57,20 +55,26 @@ function Sidebar({ setMobileOpen }) {
       <Divider />
       <List>
         <ListSubheader>Genres</ListSubheader>
-        {demoCategories.map(({ label, value }) => (
-          <Link key={value} style={{ color: theme.palette.text.primary, textDecoration: 'none' }} to="/">
-            <ListItemButton onClick={() => {}}>
-              {/* <ListItemIcon>
-                <img
-                  src="/lightLogo.png"
-                  style={{ filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)' }}
-                  height={30}
-                />
-              </ListItemIcon> */}
-              <ListItemText primary={label} />
-            </ListItemButton>
-          </Link>
-        ))}
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        ) : (
+          data.genres.map(({ name, id }) => (
+            <Link key={name} style={{ color: theme.palette.text.primary, textDecoration: 'none' }} to="/">
+              <ListItemButton onClick={() => {}}>
+                <ListItemIcon>
+                  <img
+                    src={genreIcons[name.toLowerCase()]}
+                    style={{ filter: theme.palette.mode === 'light' ? 'dark' : 'invert(1)' }}
+                    height={30}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={name} />
+              </ListItemButton>
+            </Link>
+          ))
+        )}
       </List>
     </>
   );
